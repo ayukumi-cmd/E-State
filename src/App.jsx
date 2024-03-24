2933177ca6d27515c238ca5f819786ec1ce1ae69
ayush <ayushmi274@gmail.com>
2024-03-25 00:20:31 +0530

    kk

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Signin from "./pages/signin";
-import Signup from "./pages/signup";
+import Signup from "./pages/Signup";
 import Profile from "./pages/profile";
 import About from "./pages/about"; // Change the import statement here
 export default function App() {
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function signup() {
+export default function Signup() {
   return <div>signup</div>;
 }
